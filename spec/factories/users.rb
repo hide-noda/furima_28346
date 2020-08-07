cbf7cccf5dde4cfde846adb90e8e4907ff5d8d99
hide-noda <hideyoshi.noda@gmail.com>
2020-08-07 20:57:21 +0900

    FactoryBotの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-    gimei = Gimei.name
     name                    {Faker::Name.initials(number: 2)}
     email                   {Faker::Internet.free_email}
     password                {Faker::Internet.password(min_length: 6)}
     password_confirmation   {password}
-    family_name             {Faker::Name.last_name}
-    last_name               {Faker::Name.first_name}
-    family_name_kana        {Faker::firstNameKana}
-    last_name_kana          {Faker::lastNameKana}
-    birth_day               {Faker::Date.birthday(min_age: 18, max_age: 65)}
+    family_name             {Gimei.last.kanji}
+    last_name               {Gimei.first.kanji}
+    family_name_kana        {Gimei.last.katakana}
+    last_name_kana          {Gimei.first.katakana}
+    birth_day               {1989-05-09}
   end
 end
