b4f87fa6fbeb205d0265d6953d3c5193cf9984ca
hide-noda <hideyoshi.noda@gmail.com>
2020-08-07 18:16:12 +0900

    ユーザー新規登録時のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  validates :name, presence: true
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/ 
+  validates :family_name, presence: true, format: { with: NAME_REGEX }
+  validates :last_name, presence: true, format: { with: NAME_REGEX }
+  NAME_KANA_REGEX = /\A[ァ-ン]+\z/
+  validates :family_name_kana, presence: true, format: { with: NAME_KANA_REGEX }
+  validates :last_name_kana, presence:true, format: { with: NAME_KANA_REGEX }
+  validates :birth_day, presence: true
+
 end
