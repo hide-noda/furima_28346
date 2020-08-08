bf7b0d1d18883a2ca590d3eacd1feef2f66b3268
hide-noda <hideyoshi.noda@gmail.com>
2020-08-08 16:25:09 +0900

    バリデーションのまとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :name, :family_name, :last_name, :family_name_kana, :last_name_kana, :birth_day
+  end
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  with_options format: { with: NAME_REGEX } do
+    validates :family_name, :last_name
+  end
+  NAME_KANA_REGEX = /\A[ァ-ン]+\z/.freeze
+  with_options format: { with:  NAME_KANA_REGEX } do
+    validates :family_name_kana, :last_name_kana
+  end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-  validates :name, presence: true
-  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-  validates :family_name, presence: true, format: { with: NAME_REGEX }
-  validates :last_name, presence: true, format: { with: NAME_REGEX }
-  NAME_KANA_REGEX = /\A[ァ-ン]+\z/.freeze
-  validates :family_name_kana, presence: true, format: { with: NAME_KANA_REGEX }
-  validates :last_name_kana, presence: true, format: { with: NAME_KANA_REGEX }
-  validates :birth_day, presence: true
 end
