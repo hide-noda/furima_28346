f74ba7ed14a8a82dccc628f59bf30abc412f6316
hide-noda <hideyoshi.noda@gmail.com>
2020-08-09 15:06:56 +0900

    categoryモデルの作成（active_hash）

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,18 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :shipping_address
   has_one :buyer
-  belongs_to_active_hash :category
-  belongs_to_active_hash :status
-  belongs_to_active_hash :shipping_charge
-  belongs_to_active_hash :delivery_area
-  belongs_to_active_hash :delivery_date
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :category
+    belongs_to_active_hash :status
+    belongs_to_active_hash :shipping_charge
+    belongs_to_active_hash :delivery_area
+    belongs_to_active_hash :delivery_date
+    with_options presence: true do
+      validates :image, :name, :text, :price, :category, :status, :shipping_charge, :delivery_area, :delivery_date
+    end
+    with_options numericality: { other_than: 1 } do
+      validates :category, :status, :shipping_charge, :delivery_area, :delivery_date
+    end
+
+
 end
