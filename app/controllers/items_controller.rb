cdbb4e2102e32affb00b54c3576ceef32453de0d
hide-noda <hideyoshi.noda@gmail.com>
2020-08-10 14:04:10 +0900

    各paramasの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,19 @@ class ItemsController < ApplicationController
     @items = Item.new
   end
 
+  def create
+    Item.create(item_params)
+  end
+
   private
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :price, :category_id, :status_id, :shipping_chrage_id, :delivery_area_id, :delivery_date_id).merge(user_id: current_user.id)
   end
 
 end
