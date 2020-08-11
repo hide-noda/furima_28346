bc53064b9be81c608972cd811b1a14cbc0530c5f
hide-noda <hideyoshi.noda@gmail.com>
2020-08-11 09:30:50 +0900

    エラー発生時にメッセージを表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,7 @@ class Item < ApplicationRecord
     with_options numericality: { other_than: 1 } do
       validates :category_id, :status_id, :shipping_charge_id, :delivery_area_id, :delivery_date_id
     end
+    validates :price, numericality: {greater_than_or_equal_to: 300}&&{less_than_or_equal_to: 9999999}
 
 
 end
