2b0c25a6224ad1c27e5722b1bb7d30b65482b0f0
hide-noda <hideyoshi.noda@gmail.com>
2020-08-13 19:19:03 +0900

    コードレビューに対しての修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
     name               { "商品" }
-    text               { "商品の説明です。商品の説明です。商品の説明です。商品の説明です。" }
+    text               { "商品の説明です。商品の説明です。商品の説明です。商品の説明です。商品の説明です。" }
     price              { 500 }
     category_id        { 2 }
     status_id          { 2 }
