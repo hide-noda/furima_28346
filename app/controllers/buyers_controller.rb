78a1cd88ddd03edfbfa1af412e984ca118980727
hide-noda <hideyoshi.noda@gmail.com>
2020-08-18 15:14:17 +0900

    未ログインユーザーと、出品者自身は購入ページに遷移できないように実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,11 +1,24 @@
 class BuyersController < ApplicationController
+  before_action :move_to_session
+  before_action :move_to_index
 
   def index
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    else
       @item = Item.find(params[:item_id])
+  end
+
+  private
+  def move_to_session
+    unless user_signed_in? 
+      redirect_to new_user_session_path
     end
   end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
 
 end
