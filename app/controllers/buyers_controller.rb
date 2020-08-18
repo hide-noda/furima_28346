9e47e29ad62928e8d4a62c8b5ddec6409b2320a7
hide-noda <hideyoshi.noda@gmail.com>
2020-08-18 16:25:47 +0900

    クレジットカード決済機能の実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -6,6 +6,17 @@ class BuyersController < ApplicationController
       @item = Item.find(params[:item_id])
   end
 
+  def create
+    @buyer = Buyer.new(price: order_params[:price])
+    if @buyer.valid?
+      pay_item
+      @buyer.save
+      return redirect_to root_path
+    else
+      render :show
+    end
+  end
+
   private
   def move_to_session
     unless user_signed_in? 
@@ -20,5 +31,17 @@ class BuyersController < ApplicationController
     end
   end
 
+  def order_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
 
 end
