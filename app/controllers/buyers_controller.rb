513293259b5f301317a6c5de5ecff1c16cbfd0c3
hide-noda <hideyoshi.noda@gmail.com>
2020-08-18 14:56:15 +0900

    ログインユーザーのみ購入ページへ遷移可能

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,7 +1,11 @@
 class BuyersController < ApplicationController
 
   def index
-    @item = Item.find(params[:item_id])
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    else
+      @item = Item.find(params[:item_id])
+    end
   end
-  
+
 end
